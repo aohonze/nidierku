e5151b00d14026514cc2be7407f8aa088c004e1e
aze <1161763410@qq.com>
2022-04-08 00:25:09 +0800

    feat: add vueuse as hooks utils

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,32 +1,68 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import { HomeOutlined, GoogleOutlined } from '@ant-design/icons-vue';
+import { useDark, useToggle } from '@vueuse/core';
+
+const isDark = useDark({
+  selector: '#app',
+  valueDark: 'dark',
+  valueLight: 'light',
+  onChanged(dark: boolean) {
+    // update the dom, call the API or something
+    if (dark) {
+    }
+  },
+});
+const toggleDark = useToggle(isDark);
 </script>
 
 <template>
   <div class="home-page">
-    <div class="title">nidierku</div>
-
-    <div class="discription">
-      This is a template, powered by vue3, ts, vite, antd-vue...
+    <div class="side">
+      <div class="title">nidierku</div>
     </div>
+    <div class="content">
+      <div class="discription">
+        This is a template, powered by vue3, ts, vite, antd-vue...
+      </div>
 
-    <div class="action">
-      <a-button>
-        <home-outlined />
-        <router-link to="/home"></router-link>
-      </a-button>
-      <a-button>
-        <router-link to="/about">
-          <google-outlined />
-        </router-link>
-      </a-button>
+      <div class="action">
+        <a-button target="/home">
+          <HomeOutlined />
+          Home
+        </a-button>
+        <a-button target="/about">
+          <GoogleOutlined />
+          About
+        </a-button>
+        <a-button @click="toggleDark">
+          <GoogleOutlined />
+          About
+        </a-button>
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped lang="less">
 .home-page {
+  display: flex;
+  min-height: 100vh;
+  .side {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding: 32px;
+    width: 500px;
+  }
+  .content {
+    padding: 32px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
   .title {
     font-size: 68px;
     font-family: fantasy;
